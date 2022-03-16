babb49abbef7313097a177b20912ed8b6085acdb
HolinWang <holin.wang@outlook.com>
2022-03-16 21:12:06 +0800

    CreateDoublyLinkedList

---
+++ LinkedList/DoublyLinkedList/CreateDoublyLinkedList.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: Holin Wang
+ * @Date: 2022-03-16 21:10:45
+ * @LastEditors: Holin Wang
+ * @LastEditTime: 2022-03-16 21:11:48
+ * @Description: 双向链表的实现
+ */
+function CreateDoublyLinkedList(){
+    // 内部类
+    function Node(data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+    // 属性
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+}

c5021c71bd04ac4bed98f402668993da4553a940
HolinWang <holin.wang@outlook.com>
2022-03-27 23:05:32 +0800

    实现单向循环链表的insert方法

--- LinkedList/LoopLinkedList/CreateLoopLinkedList.js
+++ LinkedList/LoopLinkedList/CreateLoopLinkedList.js
@@ -37,18 +37,26 @@ function LoopLinkedList() {
     }
     // 2. 创建新节点
     const newNode = new Node(data);
+
     if (this.length === 0) {
       this.head = newNode;
       newNode.next = this.head;
     } else {
-
+      if (position === 0) {
+        newNode.next = this.head;
+        this.head = newNode;
+      } else {
+        let current = this.head;
+        let index = 0;
+        let previous = null;
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = newNode;
+        newNode.next = current;
+      }
     }
-
-
-    if (position === 0) {
-
-    }
-
     this.length += 1;
   }
 }
@@ -58,6 +66,8 @@ const loopLinkedList = new LoopLinkedList();
 
 loopLinkedList.append("aaa");
 loopLinkedList.append("bbb");
-
-
+loopLinkedList.append("ccc");
+// loopLinkedList.insert(0, "a0");
+console.log(loopLinkedList);
+loopLinkedList.insert(1, "a3");
 console.log(loopLinkedList);
